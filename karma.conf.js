955e4be2113d1d17973f640c4dc3eb8472721182
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-06-30 15:54:29 +1200

    Fix misleading reporter output.
    - Dots are really not necessary, and ordering that so that spec is
      printed out first before progress.

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function(config) {
         ],
 
         // test results reporter to use
-        reporters: ['dots', 'progress', 'spec'],
+        reporters: ['spec', 'progress'],
 
         // web server port
         port: 9876,
